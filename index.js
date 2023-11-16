ff060d1183119ab51d69c95755418e6f5c5eb800
marvinbra <marvinbopp37@gmail.com>
2023-11-16 21:01:07 +0100

    reusable components
    react

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function Page() {
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="./react-logo.png" className="nav-logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./react-logo.png" width="40px" />
-                </nav>
-            </header>
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's a popular library, so I'll be 
@@ -16,9 +34,16 @@ function Page() {
                 <li>I'm more likely to get a job as a developer
                 if I know React</li>
             </ol>
-            <footer>
-                <small>© 2021 Ziroll development. All rights reserved.</small>
-            </footer>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     )
 }
